0ff8d13febc35a38f21c55b6ca41282f9b05cf25
rdorta <rdorta@forus.cl>
2023-03-07 22:51:43 -0300

    config init next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { SessionProvider } from 'next-auth/react'
 import { store } from '@/store/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </Provider>
   )
 }
