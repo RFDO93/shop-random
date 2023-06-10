30f3feec5cb324299085c17b9c362bad320da50c
rdorta <rdorta@forus.cl>
2023-06-09 23:05:21 -0400

    info User

--- src/components/NavbarHeaderComponents.tsx
+++ src/components/NavbarHeaderComponents.tsx
@@ -1,10 +1,20 @@
-import { BellIcon } from '@/assets/icons/BellIcon'
 import Image from 'next/image'
+import { BellIcon } from '@/assets/icons/BellIcon'
+import { useSelector } from 'react-redux'
+import { useAuth } from '@/hooks/useAuth'
 
 export const NavbarHeaderComponent = () => {
+  const { routeActive } = useSelector((state: any) => state.ui)
+
+  const { user }: { user: any } = useAuth()
+
+  const userName = `${user?.Person[0]?.name} ${user?.Person[0]?.lastName}`
+
   return (
     <nav className='w-full h-11 flex flex-row justify-between items-center'>
-      <h2 className='font-bold text-2xl text-[#252733]'>Dashboard</h2>
+      <h2 className='font-bold text-2xl text-[#252733]'>
+        {routeActive?.name || ''}
+      </h2>
 
       <div className='flex flex-row gap-3 place-items-center h-full'>
         <button
@@ -16,7 +26,7 @@ export const NavbarHeaderComponent = () => {
           <BellIcon />
         </button>
         <div className='bg-[#DFE0EB] h-9 w-[1.5px] mx-6' />
-        <p className=' font-semibold text-sm'>Richard Dorta</p>
+        <p className=' font-semibold text-sm'>{userName}</p>
         <div className=' w-[44px] h-[44px] rounded-full border-[#DFE0EB] border-solid border-2 overflow-hidden place-items-center'>
           <Image
             src='/perfil.png'
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,8 +1,12 @@
 import { LoginRequestInterface } from '@/interface/request/LoginRequestInterface'
 import { signIn, signOut, useSession } from 'next-auth/react'
+import { useMemo } from 'react'
+import Swal from 'sweetalert2'
 
 const useAuth = () => {
   const { status, data } = useSession()
+
+  const user = useMemo(() => data?.user, [data])
 
   const onLogin = async (LoginRequest: LoginRequestInterface) => {
     await signIn('credentials', {
@@ -12,14 +16,29 @@ const useAuth = () => {
     })
   }
 
-  const onLogout = async () => {
+  const confirmLogout = async () => {
     await signOut({ redirect: false, callbackUrl: '/auth/login' })
+  }
+
+  const onLogout = () => {
+    Swal.fire({
+      title: '¿Estas seguro de cerrar sesión?',
+      icon: 'warning',
+      cancelButtonText: 'No',
+      showCancelButton: true,
+      confirmButtonText: 'Si'
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        confirmLogout()
+      }
+    })
   }
 
   return {
     // Variable
     status,
-    user: data,
+    user,
     // Funciones
     onLogin,
     onLogout
